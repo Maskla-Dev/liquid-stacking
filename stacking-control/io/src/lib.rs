bc0fc7f613fdc07e0ee0de340ba54e50def76d6a
Luis Antonio Montoya Morales <devlumniom@gmail.com>
2023-06-16 00:43:56 +0000

    changes

--- stacking-control/io/src/lib.rs
+++ stacking-control/io/src/lib.rs
@@ -1,12 +1,73 @@
+#![no_std]
 use gmeta::{Metadata};
+use gstd::{msg, prelude::*, ActorId, Debug, Decode, Default, Encode, TypeInfo};
 
 pub struct StackingControl;
 
+//Metadata del smart contract
 impl Metadata for StackingControl{
     type Init = ();
-    type Handle = ();
+    type Handle = InOut<Action, ActionResult>;
     type Reply = ();
     type Others = ();
     type Signal = ();
     type State = ();
+}
+
+//Las acciones permitidas en el contrato
+pub enum Action{
+    //Stacking con monto
+    Stake(u128),
+    //Unstacking con monto
+    UnStake(u128),
+    //Retiro con monto
+    Withdraw(u128),
+    //Obtener recompensa
+    GetReward,
+}
+
+//Errores que pueden surgir en la interaccion con los contratos o en el proceso
+pub enum Error{
+    //El usuario no cuenta con VARAs suficientes para realizar Stake o Withdraw
+    InsufficientVARAs,
+    //El usuario no cuenta con VARAs para realizar accion Stake o Withdraw
+    ZeroVARAs,
+    //El usuario no cuenta con stVARAs suficientes para realizar UnStake
+    InsufficientStVARAs,
+    //El usuario no cuenta con stVARAs para realizar UnStake
+    ZeroStVaras,
+    //El usurio no cuenta con recompensas para reclamar
+    ZeroRewards,
+    //El usuario no es reconocible
+    StakerNotFound,
+    //Ha ocurrido un error al interactuar con algun contrato
+    ContractError(String),
+    //No es posible realizar la transferencia de VARAs durante Stake o Withdraw
+    TransferenceNotAllowed,
+    //La transferencia sde VARAs durante Stake o Withdraw esta siendo procesada
+    BlockedTransfer,
+    //No es una accion valida
+    ActionNotAllowed
+}
+
+//Resultados de las acciones
+pub enum ActionResult{
+    //Stackin satisfactorio, contiene el monto con el que se realizo el stacking
+    SuccessStake(u128),
+    //UnStacking satisfactorio, contiene el monto con el que se realizo el unstacking
+    SuccessUnStake(u128),
+    //Retiro satisfactorio, contiene el monto con el que se realizo el retiro
+    SuccessWithdraw(u128),
+    //Recompensa reclamada satisfactoriamente, contiene el monto con el que se realizo el reclamo
+    RewardClaimed(u128),
+    //Ha ocurrido un error, contiene el error que ocurrio
+    Unsuccessful(Error)
+}
+
+//Guarda las transacciones que se han realizado
+pub struct Transaction<T>{
+    //Id de la transaccion
+    pub id: u128;
+    //Tipo de transaccion realizada
+    pub transaction_kind: T;
 }
--- stacking-control/src/lib.rs
+++ stacking-control/src/lib.rs
@@ -1,14 +1,81 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#![no_std]
+use gstd::{msg, prelude::*, ActorId, Debug, Decode, Default, Encode, TypeInfo};
+use hashbrown::HashMap;
+use StackingAction;
+
+#[derive(Encode, Decode, TypeInfo, Debug, Default)]
+struct StackingControl{
+    //El dueño del contrato
+    admin: ActorId;
+    //El contrato para el stacking_control
+    pool_contract: ActorId;
+    //El contrato para los rendimientos
+    rewards_contract: ActorId;
+    //El contrato para stVara
+    st_vara_contrat: ActorId;
+    //Recompensas que ya han otorgadas
+    rewards_produced: u128;
+    //Recompensas por entregar
+    rewards_total: ActorId;
+    //VARAs en stacking
+    in_stacking: u128;
+    //Stackers
+    stackers: HashMap<ActorId. Stacker>
+    //Registro de transacciones
+    transactions: HashMap<ActorId, Vec<Transaction<Action>>>
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+//Estado del contrato
+static mut STACKING_CONTROL: Option<StackingControl> = None;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+//Implementaciones especificas del contrato
+impl StackingControl{
+    
+}
+
+//Inicializacion del contrato
+#[no_mangle]
+extern "C" fn init(){
+
+}
+
+//Procesa los mensajes entrantes
+#[no_mangle]
+extern "C" fn handle(){
+    let contract_state = unsafe { STACKING_CONTROL.as_ref().unwrap() }
+    //Obtiene la accion a realizar
+    let action: StackingAction = msg::load().expect("Unable to process action");
+    //Obtiene el usuario que requiere realizar la accion
+    let user = msg::source();
+    //Verifica que el usuario se encuentre en el registro
+    if let None = stackers {
+        //Si no se encuentra lo registra
+        contract_state.stackers.insert(user, Stacker::new());
     }
+    let result = match action {
+        Stake(amount) => {
+            //Se comunica con el contrato de stacking pool para realizar el deposito
+            let result: PoolResult = msg::send_for_reply_as(contract_state.pool_contract);
+        },
+        UnStake(amount) => {
+
+        },
+        Withdraw(amount) => {
+
+        },
+        GetReward => {
+
+        },
+        _ => {
+
+        }
+    }
+    //Verifica que el usuario se encuentre dentro de los registros
+    
+}
+
+//Controla las respuestas de mensajes
+#[no_mangle]
+extern "C" fn handle_reply(){
+
 }
